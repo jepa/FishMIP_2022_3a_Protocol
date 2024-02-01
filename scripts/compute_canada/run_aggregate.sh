79f13fb2d4def0296c8bc31bdf02b2b3ea9a8a30
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-02-01 14:18:23 -0300

    test parallel

--- scripts/compute_canada/run_aggregate.sh
+++ scripts/compute_canada/run_aggregate.sh
@@ -2,9 +2,8 @@
 #SBATCH --job-name=ADRD_R                                                       
 #SBATCH --account=rrg-wailung                                                   
 #SBATCH --nodes=1 # number of node MUST be 1                                    
-#SBATCH --ntasks=4                                                              
-#SBATCH --cpus-per-task=1        # number of processes                          
-#SBATCH --mem=8G                                                                
+#SBATCH --cpus-per-task=4        # number of processes                          
+#SBATCH --mem-per-cpu=8G                                                                
 #SBATCH -t 00-00:30:00                                                         
 #SBATCH --mail-user=j.palacios@oceans.ubc.ca                                    
 #SBATCH --mail-type=ALL                                                         
