cc6f97b81443be0fc16a43f9b8e99540c6ce4795
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-02-01 09:51:29 -0300

    folder re-arrange

---
+++ scripts/compute_canada/conversion_settings.R
@@ -0,0 +1,27 @@
+# Settings file for converting DBEM
+# .txt files to .Rdata files
+# Juliano Palacios
+library(parallel)
+
+scenarios <- c("fishmip3af0")
+
+category <- c("Abd")
+
+spplist <- read.table("~/projects/rrg-wailung/jepa/R/Data/Species/SppTaxonName.txt")
+
+# Partial fix for not working species
+# spplist <- dplyr::filter(spplist, V1 %in% c("603310","604708"))
+
+stryr <- 1841
+endyr <- 2010
+
+out_path <- "/home/jepa/scratch/Results/R/"
+
+# Call function
+source("/home/jepa/projects/rrg-wailung/jepa/R/Scripts/dbem_txt_to_rdata.R")
+
+
+# CAll function for scenaios in Settings file
+for(scen in 1: length(scenarios)){
+  mclapply(spplist$V1, dbem_txt_to_rdata, scenario = scenarios[scen], output_path = out_path)
+}
---
+++ scripts/compute_canada/run_conversion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash                                                                     
+#SBATCH --job-name=ADRD_R                                                       
+#SBATCH --account=rrg-wailung                                                   
+#SBATCH --nodes=1 # number of node MUST be 1                                    
+#SBATCH --ntasks=4                                                              
+#SBATCH --cpus-per-task=4        # number of processes                          
+#SBATCH --mem=3G                                                                
+#SBATCH -t 00-05:00:00                                                         
+#SBATCH --mail-user=j.palacios@oceans.ubc.ca                                    
+#SBATCH --mail-type=ALL                                                         
+
+
+# ---------------------------------------------------------------------         
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+# ---------------------------------------------------------------------         
+
+
+module load gcc/9.3.0 r/4.0.2
+export R_LIBS=~/local/R_libs/
+Rscript ./scripts/conversion_settings.R Settings.R$SLURM_ARRAY_TASK_ID
