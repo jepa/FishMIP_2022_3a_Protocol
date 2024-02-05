c7f33931161ba4544902eb2e8af4939f95698c3d
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-02-05 10:26:43 -0300

    final test run

--- scripts/compute_canada/conversion_protocol.R
+++ scripts/compute_canada/conversion_protocol.R
@@ -11,7 +11,7 @@ spplist <- read.table("~/projects/rrg-wailung/jepa/R/Data/Species/SppTaxonName.t
 # spplist <- dplyr::filter(spplist, V1 %in% c("603310","604708"))
 
 stryr <- 1841
-endyr <- 2010
+endyr <- 1851
 
 out_path <- "/home/jepa/scratch/Results/R/"
 
@@ -21,6 +21,6 @@ source(here("functions/convert_fx.R")) # Load a bunch of packages
 # Call function for scenarios in Settings file
 lapply(variables,
        convert_fx,
-       year_one = 1841,
-       year_end = 1849
+       year_one = stryr,
+       year_end = endyr
 )
