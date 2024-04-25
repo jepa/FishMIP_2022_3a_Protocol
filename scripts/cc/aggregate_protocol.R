e8f7942f7eaae7f3b7e049d95d48f9e1fea2e685
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-04-25 09:51:30 -0700

    Now catch

--- scripts/cc/aggregate_protocol.R
+++ scripts/cc/aggregate_protocol.R
@@ -13,7 +13,7 @@ load_libs(pckgs)
 # Global Variables 
 # ---------------- #
 
-varaibles <- c("Abd")
+varaibles <- c("Catch")
 
 # Scenario to call (Note this will derermine the results directory)
 scen <- "Fishing"
