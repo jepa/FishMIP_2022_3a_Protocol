525b4bea87260d732eff48be1afbbdeaacf15260
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-04-29 17:03:00 -0700

    created all netcdfs

--- scripts/netcdf_protocol.R
+++ scripts/netcdf_protocol.R
@@ -4,15 +4,21 @@ source("./functions/netcdf_convert_fx.R")
 # Read libraries
 load_libs(c("ncdf4","tidyverse","data.table")) # load these packages
 
+# ----------------------- #
+# INSTRUCTIONS FROM FISHMIP
+# ----------------------- #
+
+# We ask modellers to include all outputs from 1841 onwards 
+# for use in our evaluation assessment of model drift. 
+# Each output should be saved as two files,
+# the first covering the spin-up and transition period (1841-1960) 
+# and the second covering the historical (experiment) period (1961-2010).
+
+# histsoc	Varying direct human influences (i.e. historical estimates of fishing effort).
+# nat	No fishing (naturalized run).
+
 
 #-----------CONTROL PANNEL -------- #
-scenario <- "obsclim" # Note, use the following notation to match protocol: "pre-industrial","historical","rcp85", npp-control’ or ‘temperature-control’
-sizes <-  "tcb" #c("b10cm","b30cm","tcb")
-
-# Set the original data path (the one to be converted)
-load("/Users/jepa88/Library/CloudStorage/OneDrive-UBC/Data/FishMIP_2022_3a_Protocol/Results/Abd_data.RData")
-
-
 # Path where you want data to be saved 
 save_path <- "/Users/jepa88/Library/CloudStorage/OneDrive-UBC/Data/FishMIP_2022_3a_Protocol/Results"
 
@@ -36,8 +42,18 @@ dbem_cords <- dbem_cords %>%
     area = (4 * r_earth^2 * 0.5 * res_lon * pi/180 * cos(abs(lat) * pi/180 ) * sin(0.5 * res_lat * pi/180))
   )
 
-# Call routine for both sizes (sizes variable)
 
+
+climate_scen <- "obsclim" # Note, use the following notation to match protocol: "pre-industrial","historical","rcp85", npp-control’ or ‘temperature-control’
+sizes <-  "tc" # abundance tcb or catch tc
+socioecon_scen <- "histsoc" # Note, use nat for no fishing and histsoc for fishing
+
+# Set the original data path (the one to be converted)
+rm(combined_data)
+load("/Users/jepa88/Library/CloudStorage/OneDrive-UBC/Data/FishMIP_2022_3a_Protocol/Results/Rdata/Fishing_Catch_data.RData")
+
+
+# Call routine for both sizes (sizes variable)
 
 start_time <- c(1841,1961) # Start of time set
 end_time <- c(1960,2010) # end of time set
@@ -52,7 +68,8 @@ for(i in 1:2){
     lat_lon = dbem_cords,
     t0 = start_time[i],
     tn = end_time[i],
-    scen = scenario,
+    scen_clim = climate_scen,
+    scen_soc = socioecon_scen,
     var = sizes
   )
   
@@ -65,37 +82,53 @@ for(i in 1:2){
 
 # ---------------- TESTING FUNCTION RESULT -------------- #
 
+netcdf_files <- list.files(paste0(save_path,"/obsclim/"), full.names = F, pattern = ".nc")
 
-
-ncd_df <- metR::ReadNetCDF(paste0(save_path,"/obsclim/","dbem_gfdl-mom6_cobalt2_non_obsclim_histsoc_default_tcb_global_annual_1841-2010.nc"))
-ncd_df
-
-
-ncd_df %>% 
-  # mutate(year = year(time)) %>% 
-  # filter(time == 1)
-  group_by(time) %>% 
-  summarise(
-    mean_tcb = mean(tcb,na.rm = T)
-  ) %>% 
-  ggplot() +
-  geom_line(
-    aes(
-      x = time,
-      y = mean_tcb
+for(i in 1:length(netcdf_files)){
+  ncd_df <- metR::ReadNetCDF(paste0(save_path,"/obsclim/",netcdf_files[i]))
+  ncd_df
+  
+  
+  ncd_df %>% 
+      select(1:3,"value" = 4) %>% 
+    group_by(time) %>% 
+    summarise(
+      mean_value = mean(value,na.rm = T)
+    ) %>% 
+    ggplot() +
+    geom_line(
+      aes(
+        x = time,
+        y = mean_value
+      )
     )
+  
+  
+  ggsave(filename = paste0(save_path,"/obsclim/figures/",netcdf_files[i],"_line_plot.png"),
+         plot = last_plot()
   )
-
-ncd_df %>% 
-  mutate(year = year(time)) %>% 
-  filter(year %in% c(1841,1951,2010)) %>% 
-  ggplot() +
-  geom_tile(
-    aes(
-      x = lon,
-      y = lat,
-      fill = log10(tcb)
-    )
-  ) +
+  
+  
+  ncd_df %>% 
+    select(1:3,"value" = 4) %>% 
+    mutate(year = year(time)) %>% 
+    filter(year %in% c(start_time,end_time)) %>% 
+    ggplot() +
+    geom_tile(
+      aes(
+        x = lon,
+        y = lat,
+        fill = log10(value),
+        color = log10(value)
+      )
+    ) +
     facet_wrap(~year) +
-  scale_fill_viridis_b()
+    scale_fill_viridis_b() +
+    scale_color_viridis_b()
+  
+  ggsave(filename = paste0(save_path,"/obsclim/",netcdf_files[i],"_map_plot.png"),
+         plot = last_plot(),
+         width = 10
+  )
+  
+}
